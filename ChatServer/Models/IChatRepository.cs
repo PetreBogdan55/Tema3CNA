b3373b6f64711cd0739b8fce6788bc19b563a312
mikemike04 <mishuc04@gmail.com>
2021-05-29 20:10:39 +0300

    Added IChatRepository interface.

---
+++ ChatServer/Models/IChatRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChatCommon;
+using System.Collections.Generic;
+
+namespace chatserver.models
+{
+
+    public interface IChatRepository
+    {
+        void Add(ChatLog chatLog);
+        IEnumerable<ChatLog> GetAll();
+    }
+}
