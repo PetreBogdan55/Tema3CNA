d7489bb8084e5fa09c70b76115d2bf96ae836905
PetreBogdan55 <petrebogdan55@yahoo.com>
2021-05-30 15:49:20 +0300

    Added LoginView user control

--- ChatClientWpf/MainWindow.xaml.cs
+++ ChatClientWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatClientWpf.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace ChatClientWpf
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
     }
 }
