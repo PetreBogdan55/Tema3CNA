b9318595fb7a8efa22a3b630300672ea400070a3
mikemike04 <mishuc04@gmail.com>
2021-05-30 22:02:27 +0300

    Modified Program class.

--- ChatServer/Manager/MefManager.cs
+++ ChatServer/Manager/MefManager.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Reflection;
 
-namespace GrpcWpfSample.Server
+namespace ChatServer.Manager
 {
     public class MefManager
     {
--- ChatServerMain/Program.cs
+++ ChatServerMain/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ChatServer.Interfaces;
+using ChatServer.Logging;
+using ChatServer.Manager;
 
 namespace ChatServerMain
 {
@@ -6,7 +9,19 @@ namespace ChatServerMain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Chat service is starting. Press 'Esc' to exit.");
+
+            MefManager.Initialize();
+
+            foreach (var service in MefManager.Container.GetExportedValues<IService>())
+            {
+                service.Start();
+            }
+
+            MefManager.Container.GetExportedValue<Logger>().GetLogsAsObservable()
+                .Subscribe((x) => Console.WriteLine(x));
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
         }
     }
 }
