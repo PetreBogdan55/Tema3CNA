2eeaaa1927668af4ce56a398b5d83215403298fb
Alex N <alexneacsu2000@gmail.com>
2021-05-31 20:33:48 +0300

    Rebased client and server into one project.

---
+++ ChatServer/Grpc/ChatServiceGrpcServer.cs
@@ -0,0 +1,81 @@
+﻿using ChatApp.Common;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using GrpcWpfSample.Server.Infrastructure;
+using GrpcWpfSample.Server.Model;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcWpfSample.Server.Rpc
+{
+    [Export(typeof(IService))]
+    public class ChatServiceGrpcServer : Chat.ChatBase, IService
+    {
+        [Import]
+        private Logger m_logger = null;
+
+        [Import]
+        private ChatService m_chatService = null;
+        private readonly Empty m_empty = new Empty();
+
+        private const int Port = 50052;
+        private readonly Grpc.Core.Server m_server;
+
+        public ChatServiceGrpcServer()
+        {
+            m_server = new Grpc.Core.Server
+            {
+                Services =
+                    {
+                        Chat.BindService(this)
+                            .Intercept(new IpAddressAuthenticator())
+                    },
+                Ports =
+                    {
+                        new ServerPort("localhost", Port, ServerCredentials.Insecure)
+                    }
+            };
+
+        }
+
+        public void Start()
+        {
+            m_server.Start();
+
+            m_logger.Info("Started.");
+        }
+
+        public override async Task Subscribe(Empty request, IServerStreamWriter<ChatLog> responseStream, ServerCallContext context)
+        {
+            var peer = context.Peer; // keep peer information because it is not available after disconnection
+            m_logger.Info($"{peer} subscribes.");
+
+            context.CancellationToken.Register(() => m_logger.Info($"{peer} cancels subscription."));
+
+            try
+            {
+                await m_chatService.GetChatLogsAsObservable()
+                    .ToAsyncEnumerable()
+                    .ForEachAwaitAsync(async (x) => await responseStream.WriteAsync(x), context.CancellationToken)
+                    .ConfigureAwait(false);
+            }
+            catch (TaskCanceledException)
+            {
+                m_logger.Info($"{peer} unsubscribed.");
+            }
+        }
+
+        public override Task<Empty> Write(ChatLog request, ServerCallContext context)
+        {
+            m_logger.Info($"{context.Peer} {request}");
+
+            m_chatService.Add(request);
+
+            return Task.FromResult(m_empty);
+        }
+    }
+}
---
+++ ChatServer/Interfaces/IChatLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using ChatApp.Common;
+using System.Collections.Generic;
+
+namespace GrpcWpfSample.Server.Model
+{
+    public interface IChatLogRepository
+    {
+        void Add(ChatLog chatLog);
+        IEnumerable<ChatLog> GetAll();
+    }
+}
